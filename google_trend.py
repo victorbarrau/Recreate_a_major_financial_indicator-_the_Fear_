04f60150995fff4bbf9aa129a32f46757fd3f360
victor.B <barrauvictor111@gmail.com>
2022-10-23 22:33:28 +0200

    on peut avoir une note heure par heure sur 7 jours ou juste la dernier journée

---
+++ google_trend.py
@@ -0,0 +1,13 @@
+#import google trend data 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pytrends.request import TrendReq
+pytrend = TrendReq(hl='en-US', tz=360)
+kw_list = ["Bitcoin"]
+pytrend.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+df = pytrend.interest_over_time()
+df = df.drop(labels=['isPartial'],axis='columns')
+df.plot()
+plt.show()
